6ec4f219eb2a74dbe85e077a498b76ff850dd1d8
andrefontoura <andd.fontoura@hotmail.com>
2026-03-13 18:10:54 -0300

    Inertia + vue

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -29,10 +29,13 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        $user = $request->user();
+
         return [
             ...parent::share($request),
             'auth' => [
-                'user' => $request->user(),
+                'user' => $user,
+                'is_admin' => $user ? $user->hasRole('admin') : false,
             ],
         ];
     }
